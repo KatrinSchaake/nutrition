e10b0836119ee74337cdc2a4c25c6abdda66de36
Katrin Schaake <katrin.schaake@remoso.com>
2026-05-03 00:04:05 +0200

    add Product entity in model and correct pom.xml

--- src/main/java/de/dhbwravensburg/remoso/nutrition/model/Product.java
+++ src/main/java/de/dhbwravensburg/remoso/nutrition/model/Product.java
@@ -1,10 +1,24 @@
+//Product.java
 package de.dhbwravensburg.remoso.nutrition.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
-* Katrin Schaake,
-* TIA25,
-*  - Samstag, 02.05.2026,
-* Version: 0.1
-*/
-    public class Product {
+ * Katrin Schaake, TIA25  -  Samstag, 02.05.2026, Version: 0.1
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+	private String barcode;
+	private String name;
+	private String brand;
+	private double caloriesKcal;	// kcal / 100g
+	private double protein;			// g / 100g
+	private double carbs;			// g / 100g
+
 }
