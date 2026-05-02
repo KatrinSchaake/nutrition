28ab140e822cea2c61af668a1ffbecece6304b58
Katrin Schaake <katrin.schaake@remoso.com>
2026-05-03 00:16:33 +0200

    add HealthController

---
+++ src/main/java/de/dhbwravensburg/remoso/nutrition/controller/HealthController.java
@@ -0,0 +1,25 @@
+// HealthController.java
+package de.dhbwravensburg.remoso.nutrition.controller;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Katrin Schaake, TIA25, - Sonntag, 03.05.2026, Version: 0.1
+ */
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/api/health")
+	public Map<String, String> health() {
+
+		return Map.of(
+				"status", "UP",
+				"app", "Nutrition",
+				"version", "0.1.0"
+		);
+	}
+
+}
