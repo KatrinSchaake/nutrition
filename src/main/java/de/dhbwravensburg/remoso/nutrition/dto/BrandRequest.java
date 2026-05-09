991670300369d3c94aece211b98164be54b7bb70
Katrin Schaake <katrin.schaake@remoso.com>
2026-05-10 00:30:13 +0200

    feat(dto): add Product and Brand request/response DTOs

--- src/main/java/de/dhbwravensburg/remoso/nutrition/dto/BrandRequest.java
+++ src/main/java/de/dhbwravensburg/remoso/nutrition/dto/BrandRequest.java
@@ -1,10 +1,10 @@
 package de.dhbwravensburg.remoso.nutrition.dto;
 
 /**
-* Katrin Schaake,
-* TIA25,
-* Sonntag, 10.05.2026,
-* Version: 0.1
-*/
-    public record BrandRequest() {
-}
+ * Katrin Schaake, TIA25, Sonntag, 10.05.2026, Version: 0.1
+ */
+public record BrandRequest(
+
+		String name,
+		String countries
+) {}
