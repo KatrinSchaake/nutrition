2008c1a43ab182ad909815269b87cb9526d5cb43
Katrin Schaake <katrin.schaake@remoso.com>
2026-05-09 23:32:05 +0200

    feat(controller): adjust ProductController for new Product structure

--- src/main/java/de/dhbwravensburg/remoso/nutrition/controller/ProductController.java
+++ src/main/java/de/dhbwravensburg/remoso/nutrition/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.dhbwravensburg.remoso.nutrition.model.Brand;
 import de.dhbwravensburg.remoso.nutrition.model.Product;
 
 /**
@@ -16,10 +17,23 @@ import de.dhbwravensburg.remoso.nutrition.model.Product;
 @RequestMapping("/api/products")
 public class ProductController {
 
+	private final List<Brand> mockBrands = List.of(
+			new Brand(1L, "Olympus", "Griechenland"),
+			new Brand(2L, "Bauer", "Deutschland"),
+			new Brand(3L, "Weihenstephan", "Deutschland")
+	);
+
+	private Brand findBrand(String nameBrand) {
+		return mockBrands.stream()
+				.filter(brand -> brand.getName().equals(nameBrand))
+				.findFirst()
+				.orElseThrow();
+	}
+
 	private final List<Product> mockProducts = List.of(
-			new Product("5202178009495", "E/Feta original", "Olympus", 276, 16.5, 0.7),
-			new Product("4002334113032", "Der große Bauer - Kirsche", "Bauer", 366, 3.2, 12.0),
-			new Product("4008452010222", "Butter", "Weihenstephan", 747, 0.6, 1.0)
+			new Product("5202178009495", "E/Feta original", findBrand("Olympus"), 276, 16.5, 0.7),
+			new Product("4002334113032", "Der große Bauer - Kirsche", findBrand("Bauer"), 366, 3.2, 12.0),
+			new Product("4008452010222", "Butter",  findBrand( "Weihenstephan"), 747, 0.6, 1.0)
 	);
 
 	@GetMapping
